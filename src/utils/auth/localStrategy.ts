2dd28d558f0ae7176723d360c3b6c74efe2b22da
Elias Andres <elandmartinez+github@gmail.com>
2025-06-10 18:15:36 -0500

    created the localstrategy with passport-local

---
+++ src/utils/auth/localStrategy.ts
@@ -0,0 +1,34 @@
+import { unauthorized } from "@hapi/boom";
+import UserService from "../../services/userService";
+import { Strategy } from "passport-local"
+import { verifyPassword } from "../bcrypt.js";
+
+const userService = new UserService()
+
+const localStrategy = new Strategy({
+    usernameField: "email",
+    passwordField: "password"
+  },
+  async (email, password, done) => {
+    try {
+      const user  = await userService.getUserById(email)
+      if (!user) done(unauthorized("Error: no user matched with that email"), false)
+
+      if(user?.password) {
+        const passwordMatches = verifyPassword(password, user.password)
+        if(!passwordMatches) done(unauthorized("Error: the password is incorrect"), false)
+
+        user.password = "Inutilized Password"
+
+        done(null, user)
+      } else {
+        done(new Error("Unexpected Error: this user doesn't have a password in the database"), false)
+      }
+    } catch (error) {
+      const stringifiedError = JSON.stringify(error)
+      done(unauthorized(stringifiedError), false)
+    }
+  }
+)
+
+export default localStrategy
